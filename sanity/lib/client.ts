1783bc4424cec9dd208386c992f5446fe4fdedb0
NicolaNasr <nasr528@gmail.com>
2025-11-13 21:22:31 +0200

    remove sanity

--- sanity/lib/client.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "next-sanity";
-import { sanityConfig } from "../env";
-
-export const sanityClient = createClient({
-  projectId: sanityConfig.projectId,
-  dataset: sanityConfig.dataset,
-  apiVersion: sanityConfig.apiVersion,
-  useCdn: sanityConfig.useCdn,
-});
--- sanity/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import leader from "./leader";
-import section from "./section";
-import siteSettings from "./siteSettings";
-
-export const schemaTypes = [leader, section, siteSettings];
